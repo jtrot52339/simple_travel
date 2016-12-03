5839cea43627f5371a6a880d1137a6de840672e8
James Trotter <james@blazemeter.com>
2016-12-03 13:18:50 -0600

    fixed typo

--- simple_travel/settings.py
+++ simple_travel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ['ALOWED_HOSTS']
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 
 # Application definition
